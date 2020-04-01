315854d72f10b70f714561644f53b23ca0e81f8f
Oleg Musaev <olegmusaev@gmail.com>
2020-04-01 13:28:18 -0400

    Changed game ttl back to 3 minutes

--- src/frontend/src/conf/settings.js
+++ src/frontend/src/conf/settings.js
@@ -1,7 +1,7 @@
 const settings = {
     apiBaseUrl: process.env.REACT_APP_API_BASE_URL || "http://127.0.0.1:8080/api/v1",
     siteUrl: process.env.REACT_APP_SITE_URL || "http://127.0.0.1:3000/",
-    gameTtl: 20
+    gameTtl: 3 * 60
 };
 
 export default settings
